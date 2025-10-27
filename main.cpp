a0c81efd7404ed739f59bdf13b3271d2a821b51c
davidolofin005 <davidolofin1@gmail.com>
2025-10-27 22:48:58 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  char first, middle, last;
+  
+  first = 'D';
+  middle = 'O';
+  last = 'O';
+  
+  cout << first << middle <<last
+       << endl;
+  return 0;
+}
